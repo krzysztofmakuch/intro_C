70785c3696c19014c5acaf788fafc4908a78513e
krzysztofmakuch <55457951+krzysztofmakuch@users.noreply.github.com>
2020-03-21 18:55:30 +0100

    Add files via upload

---
+++ print1DArray.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int printArray(int array[], int size)
+{
+	/*uzupelnij mnie */
+}
+
+
+int main (void)
+{
+	int array[]={3,-5,5,12};
+	int arrayLen = sizeof(array)/sizeof(int);
+	printArray(array, arrayLen);
+}
